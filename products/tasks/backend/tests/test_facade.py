6736ab496e70b5ec6c6f68de78155f2551f9f520
Peter Kirkham <k11kirky@gmail.com>
2026-07-29 15:16:52 +0100

    fix(canvas): prepare product test dependencies
    Generated-By: PostHog Code
    Task-Id: 53286c7c-92b5-42a3-8e73-7f45d68382c8

--- products/tasks/backend/tests/test_facade.py
+++ products/tasks/backend/tests/test_facade.py
@@ -31,7 +31,6 @@ FACADE_MODULES = [
     "products.tasks.backend.facade.temporal",
     "products.tasks.backend.facade.max_tools",
     "products.tasks.backend.facade.webhooks",
-    "products.tasks.backend.facade.file_system",
 ]
 
 
